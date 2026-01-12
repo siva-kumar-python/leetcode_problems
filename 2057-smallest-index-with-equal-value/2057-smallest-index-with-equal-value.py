bd893236c9b927dcf0cbb11b1c9fba4174473353
Chennareddypalli sivakumar <sivakumar135siva@gmail.com>
2026-01-12 23:34:09 +0530

    Time: 0 ms (100%), Space: 19.3 MB (7.16%) - LeetHub

--- 2057-smallest-index-with-equal-value/2057-smallest-index-with-equal-value.py
+++ 2057-smallest-index-with-equal-value/2057-smallest-index-with-equal-value.py
@@ -1,7 +1,7 @@
 class Solution:
     def smallestEqual(self, nums: List[int]) -> int:
-        ans=float('inf')
+        
         for i in range(len(nums)):
-            if i%10==nums[i] and i<ans:
-                ans =i
-        return -1 if ans==float('inf') else ans
+            if i%10==nums[i] :
+                return i
+        return -1 
