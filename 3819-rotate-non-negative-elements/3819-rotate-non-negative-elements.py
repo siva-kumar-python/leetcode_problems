1153e7d9b5aa947ea0a7646197592771bc825076
Chennareddypalli sivakumar <sivakumar135siva@gmail.com>
2026-01-25 12:55:09 +0530

    Time: 147 ms (57.02%), Space: 35.4 MB (91.15%) - LeetHub

--- 3819-rotate-non-negative-elements/3819-rotate-non-negative-elements.py
+++ 3819-rotate-non-negative-elements/3819-rotate-non-negative-elements.py
@@ -1,18 +1,33 @@
 class Solution:
     def rotateElements(self, nums: List[int], k: int) -> List[int]:
-        di={}
-        for i,j in enumerate(nums):
-            if j>-1:
-                di[i]=j
-        ans=list(di.values())
+        # di={}
+        # for i,j in enumerate(nums):
+        #     if j>-1:
+        #         di[i]=j
+        # ans=list(di.values())
+        # if len(ans)==0:
+        #     return nums
+        # n=k%len(ans)
+        # ans=ans[n:]+ans[:n]
+        
+        # count=0
+        # for i,j in di.items():
+        #     nums[i]=ans[count]
+        #     count+=1
+        # return nums
+
+        ans=[]
+        for i in nums:
+            if i >-1:
+                ans.append(i)
         if len(ans)==0:
             return nums
         n=k%len(ans)
         ans=ans[n:]+ans[:n]
-        
         count=0
-        for i,j in di.items():
-            nums[i]=ans[count]
-            count+=1
+        for i in range(len(nums)):
+            if nums[i]>-1:
+                nums[i]=ans[count]
+                count+=1
         return nums
-        
+                
