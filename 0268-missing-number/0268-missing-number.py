58b6eb806f734a83cf769f6765b4bf2c7c86b849
Chennareddypalli sivakumar <sivakumar135siva@gmail.com>
2026-02-19 22:20:29 +0530

    Time: 15 ms (30.08%), Space: 13.4 MB (67.15%) - LeetHub

---
+++ 0268-missing-number/0268-missing-number.py
@@ -0,0 +1,12 @@
+class Solution(object):
+    def missingNumber(self, nums):
+        """
+        :type nums: List[int]
+        :rtype: int
+        """
+        nums.sort()
+        for i in range(len(nums)):
+            if i!=nums[i]:
+                return i
+        return nums[-1]+1
+        
